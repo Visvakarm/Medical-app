ee79f91c48beb02950f5b21233a34880a8c95e9a
Visvakarm <Markmedwid1@gmail.com>
2024-11-12 16:43:26 -0500

    update

---
+++ App.Clinic/ViewModels/PhysicianViewModel.cs
@@ -0,0 +1,111 @@
+﻿using Library.Clinic.Models;
+using Library.Clinic.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace App.Clinic.ViewModels
+{
+    public class PhysicianViewModel
+    {
+        public Physician? Model { get; set; }
+        public ICommand? DeleteCommand { get; set; }
+        public ICommand? EditCommand { get; set; }
+        public int Id
+        {
+            get
+            {
+                if (Model == null)
+                {
+                    return -1;
+                }
+
+                return Model.Id;
+            }
+
+            set
+            {
+                if (Model != null && Model.Id != value)
+                {
+                    Model.Id = value;
+                }
+            }
+        }
+
+        public string Name
+        {
+            get => Model?.Name ?? string.Empty;
+            set
+            {
+                if (Model != null)
+                {
+                    Model.Name = value;
+                }
+            }
+        }
+        public string LicenseNumber
+        {
+            get => Model?.LicenseNumber ?? string.Empty;
+            set
+            {
+                if (Model != null)
+                {
+                    Model.LicenseNumber = value;
+                }
+            }
+        }
+
+
+        public void SetupCommands()
+        {
+            DeleteCommand = new Command(DoDelete);
+            EditCommand = new Command((p) => DoEdit(p as PhysicianViewModel));
+        }
+
+        private void DoDelete()
+        {
+            if (Id > 0)
+            {
+                PhysicianServiceProxy.Current.DeletePhysician(Id);
+                Shell.Current.GoToAsync("//Physicians");
+            }
+        }
+
+        private void DoEdit(PhysicianViewModel? pvm)
+        {
+            if (pvm == null)
+            {
+                return;
+            }
+            var selectedPhysicianId = pvm?.Id ?? 0;
+            Shell.Current.GoToAsync($"//PhysicianDetails?physicianId={selectedPhysicianId}");
+        }
+
+        public PhysicianViewModel()
+        {
+            Model = new Physician();
+            SetupCommands();
+        }
+
+        public PhysicianViewModel(Physician? _model)
+        {
+            Model = _model;
+            SetupCommands();
+        }
+
+        public void ExecuteAdd()
+        {
+            if (Model != null)
+            {
+                PhysicianServiceProxy
+                .Current
+                .AddOrUpdatePhysician(Model);
+            }
+
+            Shell.Current.GoToAsync("//Physicians");
+        }
+    }
+}
